d8e561f07e392065c3d567e60f830e5084a6e09b
MrKamikazee <64284557+MrKamikazee@users.noreply.github.com>
2022-11-06 12:45:28 -0300

    Change Cameras

--- Egg Game/Assets/01_Scripts/Player_Movemment.cs
+++ Egg Game/Assets/01_Scripts/Player_Movemment.cs
@@ -7,6 +7,8 @@ public class Player_Movemment : MonoBehaviour
 {
     private CharacterController Player_CC;
     public float Player_Speed;
+
+    public GameObject[] Cameras = new GameObject[4];
     
     private void Awake()
     {
@@ -15,7 +17,54 @@ public class Player_Movemment : MonoBehaviour
 
     private void Update()
     {
+        Move();
+        ChangeCameras();
+    }
+
+    private void Move()
+    {
         Vector3 PlayerMove = new Vector3(Input.GetAxis("Vertical") * Player_Speed * -1, 0, Input.GetAxis("Horizontal") * Player_Speed);
         Player_CC.SimpleMove(PlayerMove * Time.deltaTime);
+    }
+
+    private void ChangeCameras()
+    {
+        if (Input.GetKeyDown(KeyCode.Q))
+        {
+            for (int i = 0; i < Cameras.Length; i++)
+            {
+                if (Cameras[i].activeSelf && (i - 1) < 0)
+                {
+                    Cameras[3].SetActive(true);
+                    Cameras[i].SetActive(false);
+                    return;
+                }
+                if (Cameras[i].activeSelf && (i - 1) >= 0)
+                {
+                    Cameras[i - 1].SetActive(true);
+                    Cameras[i].SetActive(false);
+                    return;
+                }
+            }
+        }
+
+        if (Input.GetKeyDown(KeyCode.E))
+        {
+            for (int i = 0; i < Cameras.Length; i++)
+            {
+                if (Cameras[i].activeSelf && (i + 1) > 3)
+                {
+                    Cameras[0].SetActive(true);
+                    Cameras[i].SetActive(false);
+                    return;
+                }
+                if (Cameras[i].activeSelf && (i + 1) <= 3)
+                {
+                    Cameras[i + 1].SetActive(true);
+                    Cameras[i].SetActive(false);
+                    return;
+                }
+            }
+        }
     }
 }
