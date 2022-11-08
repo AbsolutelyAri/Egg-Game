14d69ec53a0c1b50d20495e2bc8eb325ff13a8fe
MrKamikazee <64284557+MrKamikazee@users.noreply.github.com>
2022-11-08 17:56:21 -0300

    Melody Mechanic V3

--- Egg Game/Assets/01_Scripts/MelodyMechanic.cs
+++ Egg Game/Assets/01_Scripts/MelodyMechanic.cs
@@ -2,11 +2,14 @@ using System;
 using System.Collections;
 using System.Collections.Generic;
 using DG.Tweening;
+using TMPro;
 using UnityEngine;
 
 public class MelodyMechanic : MonoBehaviour
 {
     public GameObject Start_BTN;
+
+    public TextMeshProUGUI Wait_Text;
 
     private void Awake()
     {
@@ -15,9 +18,17 @@ public class MelodyMechanic : MonoBehaviour
 
     private void OnTriggerEnter(Collider other)
     {
-        if (other.CompareTag("Player"))
+        if (other.CompareTag("Player") && GameManager.GameManager_Script.CanPlay)
         {
             Start_BTN.transform.DOMoveY(35, 1f);
+        }
+        else
+        {
+            Wait_Text.text = "You can't play now, just wait " + GameManager.GameManager_Script.Time_left + " seconds to play again.";
+            Sequence WT_Sequence = DOTween.Sequence();
+            WT_Sequence.Append(Wait_Text.DOFade(1, 1f))
+                .AppendInterval(3f)
+                .Append(Wait_Text.DOFade(0, 1f));
         }
     }
 
