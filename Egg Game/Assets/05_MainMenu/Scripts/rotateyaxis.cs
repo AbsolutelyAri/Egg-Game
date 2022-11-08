04c76c7c710ca0ae6fd6b915f305e573a1dc8040
MrKamikazee <64284557+MrKamikazee@users.noreply.github.com>
2022-11-08 16:37:03 -0300

    Main Menu

---
+++ Egg Game/Assets/05_MainMenu/Scripts/rotateyaxis.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class rotateyaxis : MonoBehaviour
+{
+
+    public GameObject note;
+    public float speed = 40.0f;
+
+    void Start()
+    {
+
+        
+    }
+
+    void Update()
+    {
+        //transform.Rotate(10.0f * Time.deltaTime, 0.0f, 0.0f);
+
+        if (Input.GetKey(KeyCode.Mouse1))
+        {
+            transform.Rotate(Vector3.forward * Time.deltaTime *speed);
+        }
+        if (Input.GetKey(KeyCode.Mouse0))
+        {
+            transform.Rotate(Vector3.back * Time.deltaTime * speed);
+        }
+
+    }
+    
+}
+
